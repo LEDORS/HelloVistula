f2dd3ddb5eb788ec839c677be5c504c697c24fb3
LEDORS <michuledochowski@gmail.com>
2024-10-25 23:11:43 +0200

    test2

--- src/com/example/helloworld/HelloVistula.java
+++ src/com/example/helloworld/HelloVistula.java
@@ -3,6 +3,5 @@ package com.example.helloworld;
 public class HelloVistula {
     public static void main(String[] args) {
         System.out.println("Hello Vistula");
-        System.out.println("Hello Vistula2");
     }
 }
